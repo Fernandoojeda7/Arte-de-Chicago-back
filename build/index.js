427adf2b43faaa0cfb9c9e049977b45bf5ce2e8c
Fernando Ojeda <fernandoojeda7@gmail.com>
2023-08-25 23:11:15 -0300

    buil3

--- build/index.js
+++ build/index.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const index_routes_1 = __importDefault(require("./routes/index.routes"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config().parsed;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 3001;
+app.use((0, cors_1.default)());
 app.use("/api", index_routes_1.default);
 app.get("/", (_req, res) => {
     res.send("Ya corre el server");
--- build/utils/errorHandler.js
+++ build/utils/errorHandler.js
@@ -4,9 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Exception_1 = __importDefault(require("./Exception"));
-const registroError_1 = require("./registroError");
 function errorHandler(error, res, logout = false) {
-    (0, registroError_1.logsError)(error);
     console.log(error);
     if (error instanceof Exception_1.default) {
         return res.status(400).json({ message: error.message, logout });
